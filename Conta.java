372f9a1937ca566b49602e9ee67a3c18e483b42e
Leonardo <leonarddo.gomees@gmail.com>
2023-01-21 18:16:40 -0300

    Estudando java

--- Conta.java
+++ Conta.java
@@ -3,4 +3,11 @@ public class Conta {
     int agencia;
     int numero;
     String titular;
+
+    //Maneira de depositar dinheiro em uma conta
+
+    public void deposita( double valor) {
+        this.saldo = this.saldo + valor;
+
+    } 
 }
