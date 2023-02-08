89f90f542f63465ab566d7e805fc7f96f0b0d41a
Leonardo Gomes <113685768+Leeogmz@users.noreply.github.com>
2023-02-08 12:34:39 -0300

    Update Funcionario.java

--- ByteBankHerdado/Funcionario.java
+++ ByteBankHerdado/Funcionario.java
@@ -3,13 +3,6 @@ public class Funcionario {
     private String cpf;
     private double salario;
 
-    public Funcionario(String nome, String cpf, double salario){
-    this.nome = nome;
-    this.cpf = cpf;
-    this.salario = salario;
-
-    }
-
     public double getBonificacao(){
         return this.salario * 0.1;
     }
