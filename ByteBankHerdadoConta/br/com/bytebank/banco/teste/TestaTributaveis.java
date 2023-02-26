b9482f53331ec8d9dce536e788f2ca54917bba7c
Leonardo <leonarddo.gomees@gmail.com>
2023-02-26 17:20:17 -0300

    Estudando Java (package)

--- ByteBankHerdadoConta/br/com/bytebank/banco/teste/TestaTributaveis.java
+++ ByteBankHerdadoConta/br/com/bytebank/banco/teste/TestaTributaveis.java
@@ -1,3 +1,8 @@
+package br.com.bytebank.banco.teste;
+import br.com.bytebank.banco.modelo.CalculadorImposto;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SeguroDeVida;
+
 public class TestaTributaveis {
     public static void main(String[] args) {
         ContaCorrente cc = new ContaCorrente(297, 10055);
