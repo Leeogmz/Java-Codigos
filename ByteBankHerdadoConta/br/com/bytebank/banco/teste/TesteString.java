59407031ca115e29f176d3e3597422a7d54d073a
Leonardo <leonarddo.gomees@gmail.com>
2023-03-01 22:43:10 -0300

    Estudando Java (conhecendo a classe String)

---
+++ ByteBankHerdadoConta/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,18 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3;
+		String nome = "Alura"; //objeto literal
+		//String outro = new String("Alura");
+		
+		
+		
+		System.out.println(nome);
+
+	}
+
+}
