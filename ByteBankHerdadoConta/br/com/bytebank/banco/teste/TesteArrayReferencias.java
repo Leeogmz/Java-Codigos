dc26abf55dbad11b4250ff495c990e14e21dd33b
Leonardo <leonarddo.gomees@gmail.com>
2023-03-08 19:26:33 -0300

    Estudando Java (Array)

--- ByteBankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ ByteBankHerdadoConta/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,21 +1,32 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
       
-        ContaCorrente[] contas = new ContaCorrente[5];
+        Object[] referencias = new Object[5];
 
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        contas[0] = cc1;
+        referencias[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente (123,321);
-        contas[1] = cc2;
+        ContaPoupanca cp2 = new ContaPoupanca (123,321);
+        referencias[1] = cp2;
 
-        System.out.println(contas[1].getNumero());
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
+
+        
+
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
+        System.out.println(cp2.getNumero());
+        System.out.println(ref.getNumero());
+
 
     }
 
